86e9f567f71591d07220e9fac2c08f464889a051
Asrar Ahammad <asrarahammadshaik@gmail.com>
2024-07-19 06:38:38 +0530

    completed appwrite configuration

---
+++ 12Blog_appwrite/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  return (
+    <>
+      <div className="h-screen w-full bg-zinc-900"></div>
+    </>
+  )
+}
+
+export default App
